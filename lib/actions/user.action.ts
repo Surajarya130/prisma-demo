f34bdef442553138f7b377f3d2f910d52b043c33
surajarya130 <surajarya130@gmail.com>
2023-11-06 01:14:48 +0530

    FIx: Edit User feature

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,82 @@
+// This will be a Next.js server action file
+"use server";
+
+// Import the model and the Prisma client
+import { User } from "@prisma/client";
+import prisma from "@/prisma";
+
+// READ 1: Get all users
+export async function getAllUsers() {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    throw new Error("Failed to fetch all users");
+  }
+}
+
+// Read 2: Get one user
+export async function getUserById(id: string) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: { id },
+    });
+    return user;
+  } catch (error) {
+    throw new Error("Failed to fetch the user");
+  }
+}
+
+// Create: Create a user
+export const createUser = async (params: User) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name: params.name as string,
+        email: params.email as string,
+      },
+    });
+    return newUser;
+  } catch (error) {
+    throw new Error("An error occurred while creating the user");
+  }
+};
+
+// UPDATE: Update a user
+export async function updateUser(field: User, id: string) {
+  try {
+    // First, find the user
+    const selectedUser = await prisma.user.findFirst({
+      where: { id },
+    });
+    // Then, update the user
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: selectedUser?.id,
+      },
+      data: field,
+    });
+    return updatedUser;
+  } catch (error) {
+    throw new Error("User not updated");
+  }
+}
+
+// DELETE: Delete a user
+export const deleteUser = async (id: string) => {
+  try {
+    // First find the relevant user
+    const selectedUser = await prisma.user.findFirst({
+      where: { id },
+    });
+
+    // Then, delete the user
+    const deletedUser = await prisma.user.delete({
+      where: { id },
+    });
+
+    return deletedUser;
+  } catch (error) {
+    throw new Error("User not deleted");
+  }
+};
---
+++ prisma/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // This line declares a global variable that is used to store the prisma client instance
+  var prisma: PrismaClient | undefined;
+}
+
+// This line checks if a prisma client instance already exists in the global variable. If it does, it uses that instance. If it doesn't, it creates a new Prisma client instance.
+const prisma = global.prisma || new PrismaClient();
+
+// This line assigns the Prisma client instance to the global variable only if the application is running in the devlopment environment. This is done to ensure that a new Prisma client instance is created every time the application is started in the development environment.
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
+}
+
+export default prisma;
